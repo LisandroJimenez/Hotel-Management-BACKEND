73b88a31ccd7cea65643e5d8ced0bccfac212644
LisandroJimenez <ljimenezv2006@gmail.com>
2025-04-28 15:57:34 -0600

    server changes

--- configs/server.js
+++ configs/server.js
@@ -4,14 +4,19 @@ import cors from 'cors'
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
+import limiter from '../src/middlewares/validate-cant-request.js';
 
 const middlewares = (app) =>{
     app.use(express.urlencoded({extended: false}));
     app.use(express.json());
-
     app.use(cors());
     app.use(helmet());
     app.use(morgan('dev'));
+    app.use(limiter)
+}
+
+const routes = (app) =>{
+
 }
 
 const conectarDB = async() =>{
@@ -25,10 +30,11 @@ const conectarDB = async() =>{
 
 export const initServer = async() =>{
  const app = express();
- const port = process.env.PORT || 3000;
+ const port = process.env.PORT || 3001;
  try {
      middlewares(app);
      conectarDB();
+     routes(app);
      app.listen(port);
      console.log(`server running on port ${port}`)
     
