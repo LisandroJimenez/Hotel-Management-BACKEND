e56b885f3be6fdbe0acc4d8444bf1ff605094e80
Informatica <informatica@alumno.local>
2025-04-28 17:18:07 -0600

    resolver errore de conestar ala base de datos

---
+++ src/users/user.controller.js
@@ -0,0 +1,94 @@
+import { response } from "express";
+import { hash } from 'argon2';
+import User from "./user.model.js";
+
+export const getUsers = async (req = request, res = response) => {  
+    try {
+        const {limite = 10, desde = 0 } = req.query;
+        const query = {status: true}
+        const [total, users] = await Promise.all([
+            User.countDocuments(query),
+            User.find(query)
+                .skip(Number(desde))
+                .limit(Number(limite))
+        ])
+
+        res.status(200).json({
+            success: true,
+            total,
+            users
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            msg: "Error al obtener usuarios",
+            error
+        })
+    }
+}
+export const updateUser = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const { _id, password, email, ...data } = req.body;
+      const user = await User.findById(id);
+      if (!user) {
+          return res.status(404).json({
+              success: false,
+              msg: 'User not found'
+          });
+      }
+      if (password) {
+          user.password = await hash(password);
+      }
+      if(email){
+        user.email = email;
+      }
+
+      Object.assign(user, data);
+
+      const updateUser = await user.save();
+            
+        res.status(200).json({
+            success: true,
+            msg: "User update successfully!",
+            user: updateUser
+        })
+
+  } catch (error) {
+      console.error(error); 
+      res.status(500).json({
+          success: false,
+          msg: 'Error when updating user',
+          error
+      });
+  }
+};
+
+
+
+export const deleteUser = async (req, res) => {
+  try {
+      const { id } = req.params;
+
+      const user = await User.findByIdAndUpdate(id, { status: false }, { new: true });
+      if (!user) {
+          return res.status(404).json({
+              success: false,
+              msg: "User not found"
+          });
+      }
+      res.status(200).json({
+          success: true,
+          msg: 'User disabled',
+          user,
+          authenticatedUser
+      });
+  } catch (error) {
+      res.status(500).json({
+          success: false,
+          msg: 'Error deactivating user',
+          error
+      });
+  }
+};
---
+++ src/users/user.routes.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { deleteUser, getUsers, updateUser } from "./user.controller.js";
+import { existUserById } from "../helpers/db-validator.js";
+import { validateFields } from "../middlewares/validate-fields.js";
+import { validateJWT } from "../middlewares/validate-jwt.js";
+import { validateOwner, validateEmail, validateRole } from "../middlewares/validate-user.js";
+
+const router = Router()
+
+router.get("/", getUsers)
+
+router.put(
+    "/:id",
+    [
+        validateJWT,
+        check("id", "Not a valid ID").isMongoId(),
+        check("id").custom(existUserById),
+        validateRole,
+        validateEmail,
+        validateFields
+    ],
+    updateUser
+
+)
+
+router.delete(
+    "/:id",
+    [
+        validateJWT,
+        check("id", "Not a valid ID").isMongoId(),
+        check("id").custom(existUserById),
+        validateOwner,
+        validateFields
+    ],
+    deleteUser
+)
+
+export default router;
