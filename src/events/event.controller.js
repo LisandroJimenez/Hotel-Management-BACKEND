d2bff10cbab42d19946dab1bf77a8cbcc7e13ad5
JoaquinFigueroa5 <hfigueroa-2023417@kinal.edu.gt>
2025-05-28 09:19:54 -0600

    Events modified

--- src/events/event.controller.js
+++ src/events/event.controller.js
@@ -7,7 +7,7 @@ export const saveEvent = async (req, res) => {
         const data = req.body;
 
         console.log(data);
-        
+
 
         const event = new Event({ ...data });
         await event.save();
@@ -17,8 +17,12 @@ export const saveEvent = async (req, res) => {
 
         const eventWithDetails = {
             ...event.toObject(),
-            hotel: hotel ? { HotelName: hotel.name } : 'Data not found',
-            room: room ? { NumberRoom: room.numberRoom } : 'Data not found',
+            hotel: hotel
+                ? { HotelId: hotel._id, HotelName: hotel.name }
+                : 'Data not found',
+            room: room
+                ? { RoomId: room._id, NumberRoom: room.numberRoom }
+                : 'Data not found',
             date: event.date.toISOString().split('T')[0]
         };
 
@@ -40,20 +44,20 @@ export const saveEvent = async (req, res) => {
 
 export const getEvents = async (req, res) => {
     const { limit = 10, offset = 0 } = req.query;
-    const query = { status: true}
+    const query = { status: true }
 
-    try{
+    try {
         const events = await Event.find(query)
-        .skip(Number(offset))
-        .limit(Number(limit))
+            .skip(Number(offset))
+            .limit(Number(limit))
 
         const eventWithHotel = await Promise.all(events.map(async (event) => {
             const hotel = await Hotel.findById(event.hotel);
             const room = await Room.findById(event.room);
             return {
                 ...event.toObject(),
-                hotel: hotel ? { HotelName: hotel.name} : 'Data not found',
-                room: room ? { NumberRoom: room.numberRoom } : 'Data not found'
+                hotel: hotel ? { HotelName: hotel.name, _id: hotel._id } : 'Data not found',
+                room: room ? { NumberRoom: room.numberRoom, _id: room._id } : 'Data not found'
             }
         }))
 
@@ -64,7 +68,7 @@ export const getEvents = async (req, res) => {
             total,
             hotel: eventWithHotel
         })
-    }catch(error){
+    } catch (error) {
         return res.status(500).json({
             success: false,
             msg: 'Error getting events',
@@ -76,9 +80,15 @@ export const getEvents = async (req, res) => {
 export const updateEvent = async (req, res) => {
     try {
         const { id } = req.params;
-        const { _id, ...data } = req.body;
+        const { ...data } = req.body;
 
-        const updatedEvent = await Event.findByIdAndUpdate(id, data, { new: true});
+        console.log('ID: ', id, " Data: ", data);
+
+
+        const updatedEvent = await Event.findByIdAndUpdate(id, data, { new: true });
+
+        console.log('si llego');
+
 
         res.status(200).json({
             success: true,
@@ -86,7 +96,8 @@ export const updateEvent = async (req, res) => {
             event: updatedEvent
         })
 
-    }catch(error){
+    } catch (error) {
+        console.error(error);
         return res.status(500).json({
             success: false,
             msg: 'Error updating event',
@@ -97,14 +108,15 @@ export const updateEvent = async (req, res) => {
 
 export const deleteEvent = async (req, res) => {
     const { id } = req.params;
-    try{
-        await Event.findByIdAndUpdate(id, { status: false});
+    try {
+        await Event.findByIdAndUpdate(id, { status: false });
         return res.status(200).json({
             success: true,
             msg: 'Event deleted successfully'
         })
 
-    }catch(error){
+    } catch (error) {
+
         return res.status(500).json({
             success: false,
             message: 'Error deleting room',
